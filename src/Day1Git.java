f02901fa98d60139eb5766f4dd46d12b476eb50e
tubaulker <111124657+tubaulker@users.noreply.github.com>
2022-08-17 20:00:40 +0300

    Update Day1Git.java

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -7,5 +7,6 @@ public class Day1Git {
         System.out.println("Hello Git 3. Deneme");
         System.out.println("Hello Git 4. Deneme");
         System.out.println("Hello Git 5. Deneme");
+        System.out.println("Hello Git 6. Deneme");
     }
 }
